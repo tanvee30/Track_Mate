891681c95fc292a19d4405716e89c03622139e40
tanvee30 <tanvee2331155@akgec.ac.in>
2025-12-04 19:54:53 +0530

    update settings.py

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,29 +1,8 @@
-#
-#
-#
-# from rest_framework import serializers
-# from .models import User
-#
-# class SignupSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#
-# class VerifyOTPSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     code = serializers.CharField()
-#
-# class LoginSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     password = serializers.CharField()
-#
-# class ForgotPasswordSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#
-# class ResetPasswordSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     code = serializers.CharField()
-#     new_password = serializers.CharField()
+
 
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class SignupSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -45,3 +24,19 @@ class ResetPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
     code = serializers.CharField()
     new_password = serializers.CharField(write_only=True)
+
+# ----------------------------------------------------
+# LOGOUT SERIALIZER (new)
+# ----------------------------------------------------
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.token = attrs["refresh"]
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except Exception:
+            self.fail("Invalid or expired refresh token")
--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,52 +1,4 @@
-# # # #
-# # # #
-# # # #
-# # # # from django.urls import path
-# # # # from . import views
-# # # #
-# # # # urlpatterns = [
-# # # #     path("signup/", views.signup),
-# # # #     path("verify/", views.verify_otp),
-# # # #     path("login/", views.login),
-# # # #     path("forgot-password/", views.forgot_password),
-# # # #     path("reset-password/", views.reset_password),
-# # # # ]
-# # #
-# # #
-# # # from django.urls import path
-# # # from . import views
-# # #
-# # # urlpatterns = [
-# # #     path("signup/", views.signup),
-# # #     path("verify-otp/", views.verify_otp),
-# # #     path("login/", views.login),
-# # #     path("refresh/", views.refresh_token),
-# # #     path("forgot-password/", views.forgot_password),
-# # #     path("reset-password/", views.reset_password),
-# # # ]
-# #
-# # from django.urls import path
-# # from . import views
-# #
-# # urlpatterns = [
-# #     path("signup/", views.signup),
-# #     path("verify-otp/", views.verify_otp),
-# #     path("login/", views.login_view),
-# #     path("refresh/", views.refresh_token),
-# #     path("forgot-password/", views.forgot_password),
-# #     path("reset-password/", views.reset_password),
-# # ]
-#
-# from django.urls import path
-# from . import views
-#
-# urlpatterns = [
-#     path("signup/", views.signup),
-#     path("verify/", views.verify_otp),
-#     # path("login/", views.login),
-#     path("forgot-password/", views.forgot_password),
-#     path("reset-password/", views.reset_password),
-# ]
+
 
 from django.urls import path
 from . import views
@@ -58,4 +10,6 @@ urlpatterns = [
     path("refresh/", views.refresh_token),
     path("forgot-password/", views.forgot_password),
     path("reset-password/", views.reset_password),
+    path("logout/", views.logout_view),
+
 ]
